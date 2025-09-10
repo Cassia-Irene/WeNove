d71fddbf395c156755a65953f66d128373243607
Cássia Irene <157053868+Cassia-Irene@users.noreply.github.com>
2025-09-10 14:12:17 -0300

    Atualizar o page.tsx
    Adicionando o header da seção 'Sobre-nos'

---
+++ src/app/sobre-nos/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import Header_Produto from "@/components/Header-produto"
+
+
+export default function Sobre_nos_Page() {
+
+  return (
+    <div className="min-h-screen bg-[#FFF]">
+      
+      {/* Header */}
+      <Header_Produto />
+    {/* Footer */}
+        <Footer />
+    </div>
+  )
+}
